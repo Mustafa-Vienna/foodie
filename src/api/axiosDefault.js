6377f1e5b64862eacb67e144b30a323d4a82b755
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-06 13:35:26 +0000

    Removed withCredentials from Axios to align with backend authentication settings

--- src/api/axiosDefault.js
+++ src/api/axiosDefault.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || 'https://foodieback.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export default axios;
