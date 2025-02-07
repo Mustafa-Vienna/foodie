c61e640748fd38ef4f595081e08aa1da864a2d8a
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-07 16:12:51 +0000

    Added full Cloudinary url for the profile image

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,7 +3,8 @@ import styles from '../styles/Avatar.module.css'
 
 const Avatar = ({ src, height = 45, text }) => {
   return <span>
-    <img className={styles.Avatar} src={src}
+    <img className={styles.Avatar}
+      src={src || "https://res.cloudinary.com/duemxeswe/image/upload/v1737306346/default_profile_girwrs.jpg"} 
       height={height} width={height} alt="avatar" />
     {text}
   </span>
