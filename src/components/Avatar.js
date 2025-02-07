1dd48118499dd7fc342edb64e47e88c457fae651
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-02-07 20:58:02 +0000

    Refactored Avatar component to improve readability and maintainability

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,12 +2,15 @@ import React from 'react'
 import styles from '../styles/Avatar.module.css'
 
 const Avatar = ({ src, height = 45, text }) => {
-  return <span>
-    <img className={styles.Avatar}
-      src={src || "https://res.cloudinary.com/duemxeswe/image/upload/v1737306346/default_profile_girwrs.jpg"} 
-      height={height} width={height} alt="avatar" />
-    {text}
-  </span>
+  const defaultAvatar =
+    "https://res.cloudinary.com/duemxeswe/image/upload/v1737306346/default_profile_girwrs.jpg";
+  return (
+    <span>
+      <img className={styles.Avatar} src={src || defaultAvatar}
+        height={height} width={height} alt="avatar" />
+      {text}
+    </span>
+  );
 };
 
 export default Avatar;
