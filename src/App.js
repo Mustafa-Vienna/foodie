2f0bd19866ff4a1dbe992eb1c2b13439f9158675
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-11 15:21:24 +0100

    Add PostsList and PostsPage routes to App.js for post browsing and details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './api/axiosDefault';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
+import PostsPage from './pages/posts/PostsPage';
+import PostsList from './pages/posts/PostsList';
 
 
 function App() {
@@ -15,9 +17,11 @@ function App() {
       <Container className={styles.Main}>
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/feed" element={<PostsList />} />
           <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/signin" element={<SignInForm />} />
           <Route path="/posts/create" element={<PostCreateForm />} />
+          <Route path="/posts/:id" element={<PostsPage />} />
           <Route path="*" element={<h1>Page not found!</h1>} />
         </Routes>
       </Container>
