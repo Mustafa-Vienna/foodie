3605c5e10f7bc8d594920c0fe2af6a2f48e9d4b0
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-15 02:48:27 +0100

    Update App.js to include HomePage route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// src/App.js
+import React from "react";
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
@@ -8,25 +10,27 @@ import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostsPage from './pages/posts/PostsPage';
 import PostsList from './pages/posts/PostsList';
-
+import HomePage from './pages/posts/HomePage';
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
 function App() {
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Routes>
-          {/* <Route path="/" element={<h1>Home Page</h1>} /> */}
-          <Route path="/" element={<PostsList />} />
-          <Route path="/feed" element={<PostsList />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/signin" element={<SignInForm />} />
-          <Route path="/posts/create" element={<PostCreateForm />} />
-          <Route path="/posts/:id" element={<PostsPage />} />
-          <Route path="*" element={<h1>Page not found!</h1>} />
-        </Routes>
-      </Container>
-    </div>
+    <CurrentUserProvider>
+      <div className={styles.App}>
+        <NavBar />
+        <Container className={styles.Main}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/feed" element={<PostsList />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/signin" element={<SignInForm />} />
+            <Route path="/posts/create" element={<PostCreateForm />} />
+            <Route path="/posts/:id" element={<PostsPage />} />
+            <Route path="*" element={<h1>Page not found!</h1>} />
+          </Routes>
+        </Container>
+      </div>
+    </CurrentUserProvider>
   );
 }
 
