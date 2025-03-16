8f8d246b209a8a0c183777490e189bc7aa3aa8c4
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-16 16:36:28 +0100

    Update App.js to reference PostDetailPage and PostListPage for better clarity.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import './api/axiosDefault';
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
-import PostsPage from './pages/posts/PostsPage';
-import PostsList from './pages/posts/PostsList';
+import PostDetailPage from './pages/posts/PostDetailPage';
+import PostListPage from './pages/posts/PostListPage';
 import HomePage from './pages/posts/HomePage';
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
@@ -21,11 +21,11 @@ function App() {
         <Container className={styles.Main}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/feed" element={<PostsList />} />
+            <Route path="/feed" element={<PostListPage />} />
             <Route path="/signup" element={<SignUpForm />} />
             <Route path="/signin" element={<SignInForm />} />
             <Route path="/posts/create" element={<PostCreateForm />} />
-            <Route path="/posts/:id" element={<PostsPage />} />
+            <Route path="/posts/:id" element={<PostDetailPage />} />
             <Route path="*" element={<h1>Page not found!</h1>} />
           </Routes>
         </Container>
