7683652369b58fce90e1a554184b566a8856d9d6
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-23 04:17:02 +0100

    Replace social stats styling with sharedStyles module

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
 import LikeButton from "../pages/likes/LikeButton";
 import styles from "../styles/PostCard.module.css";
+import sharedStyles from "../styles/SharedStyles.module.css";
 
 const PostCard = ({
   id,
@@ -35,19 +36,22 @@ const PostCard = ({
         <Card.Text className={styles.postText}>
           {introduction.substring(0, 100) + (introduction.length > 100 ? "..." : "")}
         </Card.Text>
-        <div className={styles.socialStats}>
+        <div className={sharedStyles.socialStats}>
           {isLoggedIn ? (
-            <div className={styles.statItem}>
+            <div className={sharedStyles.statItem}>
               <LikeButton postId={id} likesCount={likesCount} setLikesCount={setLikesCount} />
             </div>
           ) : (
-            <span onClick={() => handleWarning("like")} className={`${styles.statItem} ${styles.notLoggedIn}`}>
+            <span
+              onClick={() => handleWarning("like")}
+              className={`${sharedStyles.statItem} ${sharedStyles.notLoggedIn}`}
+            >
               <i className="fa-regular fa-heart"></i> {likesCount}
             </span>
           )}
           <span
             onClick={!isLoggedIn ? () => handleWarning("comment") : undefined}
-            className={`${styles.statItem} ${!isLoggedIn ? styles.notLoggedIn : ""}`}
+            className={`${sharedStyles.statItem} ${!isLoggedIn ? sharedStyles.notLoggedIn : ""}`}
           >
             <i className="fa-regular fa-comment"></i> {comments_count}
           </span>
