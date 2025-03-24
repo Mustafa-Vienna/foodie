c4c6faa496a228ccd96f0e1087b85ee51464051c
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 02:28:37 +0100

    Update Asset.js to use shared flexColumnCenter and message styles from SharedStyles.module.css"

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
+import sharedStyles from "../styles/SharedStyles.module.css";
 
 const Asset = ({ spinner, src, message = "" }) => {
   return (
-    <div className={`${styles.Asset} p-4`}>
+    <div className={`${styles.asset} ${sharedStyles.flexColumnCenter} p-4`}>
       {spinner && <Spinner animation="border" />}
-      {src && <img src={src} alt={message} className={styles.Image} />}
-      {message && <p className="mt-4">{message}</p>}
+      {src && <img src={src} alt={message} className={styles.image} />}
+      {message && <p className={`mt-4 ${sharedStyles.message}`}>{message}</p>}
     </div>
   );
 };
