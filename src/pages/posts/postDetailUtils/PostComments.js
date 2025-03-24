35d3f086f6b8100607145b63620c5f5c90fd4606
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 18:02:09 +0100

    Refactored PostComments to support smooth scroll using forwardRef and location state; added shared styles for consistent layout.

--- src/pages/posts/postDetailUtils/PostComments.js
+++ src/pages/posts/postDetailUtils/PostComments.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { forwardRef, useEffect } from "react";
 import { Card } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 import CommentList from "../../comments/CommentList";
 import styles from "../../../styles/PostDetailPage.module.css";
+import sharedStyles from "../../../styles/SharedStyles.module.css";
 
-const PostComments = ({ postId, currentUser }) => {
+const PostComments = forwardRef(({ postId, currentUser }, ref) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollToComments && ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [location.state, ref]);
+
   return (
-    <Card className={`${styles.postCard} mt-4`}>
-      <Card.Body>
-        <CommentList postId={postId} currentUser={currentUser} />
-      </Card.Body>
-    </Card>
+    <div ref={ref} id="comments" className={`${styles.commentsContainer} ${sharedStyles.commentsContainer}`}>
+      <Card className={`${styles.commentsCard} ${sharedStyles.baseCard}`}>
+        <Card.Body>
+          <CommentList postId={postId} currentUser={currentUser} />
+        </Card.Body>
+      </Card>
+    </div>
   );
-};
+});
 
 export default PostComments;
