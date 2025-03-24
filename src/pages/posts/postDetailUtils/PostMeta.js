6e42bbda6a3eaabf8ebd12d8437eb97161ae54a9
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 21:34:56 +0100

    Update PostMeta to add conditional LikeButton, style category with metaValue, use link--orange for tags, and add comment link

--- src/pages/posts/postDetailUtils/PostMeta.js
+++ src/pages/posts/postDetailUtils/PostMeta.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useCurrentUser } from "../../../contexts/CurrentUserContext";
 import LikeButton from "../../likes/LikeButton";
 import styles from "../../../styles/PostDetailPage.module.css";
 import sharedStyles from "../../../styles/SharedStyles.module.css";
 
 const PostMeta = ({ post, likesCount, setLikesCount, postId }) => {
+  const currentUser = useCurrentUser();
+
   return (
     <div className={styles.postMetaContainer}>
       <div className={styles.postMeta}>
-        <strong>Category:</strong> {post.category}
+        <strong>Category:</strong>{" "}
+        <span className={styles.metaValue}>{post.category}</span>
       </div>
 
       {post.tags && post.tags.length > 0 && (
@@ -17,7 +21,7 @@ const PostMeta = ({ post, likesCount, setLikesCount, postId }) => {
           {post.tags.map((tag, index) => (
             <span key={tag.id}>
               {index > 0 && ", "}
-              <Link to={`/posts/?tags=${tag.id}`} className={styles.postTag}>
+              <Link to={`/posts/?tags=${tag.id}`} className={`${styles.postTag} ${sharedStyles["link--orange"]}`}>
                 {tag.name}
               </Link>
             </span>
@@ -27,12 +31,26 @@ const PostMeta = ({ post, likesCount, setLikesCount, postId }) => {
 
       <div className={sharedStyles.socialStats}>
         <div className={sharedStyles.statItem}>
-          <LikeButton
-            postId={postId}
-            likesCount={likesCount}
-            setLikesCount={setLikesCount}
-            likeId={post.like_id}
-          />
+          {currentUser ? (
+            <LikeButton
+              postId={postId}
+              likesCount={likesCount}
+              setLikesCount={setLikesCount}
+              likeId={post.like_id}
+            />
+          ) : (
+            <Link to="/signin" className={sharedStyles["link--blue"]}>
+              <i className="fa-regular fa-heart"></i> {likesCount}
+            </Link>
+          )}
+        </div>
+        <div className={sharedStyles.statItem}>
+          <Link 
+            to={`/posts/${postId}#comments`} 
+            className={sharedStyles.commentLink}
+          >
+            <i className="fa-regular fa-comment"></i> {post.comments_count}
+          </Link>
         </div>
       </div>
     </div>
