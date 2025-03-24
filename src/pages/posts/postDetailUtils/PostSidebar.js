bf56c38e5c57505662582a65f9786159fb5e2a05
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 18:09:05 +0100

    Refactor PostSidebar to apply shared baseCard style for consistent card layout

--- src/pages/posts/postDetailUtils/PostSidebar.js
+++ src/pages/posts/postDetailUtils/PostSidebar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import styles from "../../../styles/PostDetailPage.module.css";
+import sharedStyles from "../../../styles/SharedStyles.module.css";
 
 const PostSidebar = () => {
   return (
-    <Card className={styles.sidebarCard}>
+    <Card className={`${styles.sidebarCard} ${sharedStyles.baseCard}`}>
       <Card.Body>
         <h5 className={styles.sidebarTitle}>Popular Profiles</h5>
         <p>(Content to be added)</p>
