bc0ca0540802de5ec0cff68928ad93f9aa811a0f
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 14:50:58 +0100

    Update Comment.js to use shared commentsContainer, flexSpaceBetween, flexCenter, circle, and backgroundTransition styles from SharedStyles.module.css

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "../../styles/Comment.module.css";
+import sharedStyles from "../../styles/SharedStyles.module.css";
 
 const Comment = ({ comment }) => {
+  const { author, content, created_at, profile_image, profile_id } = comment;
+
   return (
-    <div className={styles.commentItem}>
-      <div className={styles.commentHeader}>
-        <span className={styles.commentAuthor}>{comment.author}</span>
-        <span className={styles.commentDate}>{comment.created_at}</span>
+    <div className={`${styles.commentContainer} ${sharedStyles.commentsContainer} ${sharedStyles.backgroundTransition}`}>
+      <div className={`${styles.commentHeader} ${sharedStyles.flexSpaceBetween}`}>
+        <Link to={`/profiles/${profile_id}`} className={`${styles.commentAuthor} ${sharedStyles.flexCenter}`}>
+          {profile_image && (
+            <img src={profile_image} alt={author} className={`${styles.authorImage} ${sharedStyles.circle}`} />
+          )}
+          <span>{author}</span>
+        </Link>
+        <span className={styles.commentDate}>
+          {new Date(created_at).toLocaleString()}
+        </span>
       </div>
-      <p className={styles.commentText}>{comment.content}</p>
+      <div className={styles.commentContent}>{content}</div>
     </div>
   );
 };
