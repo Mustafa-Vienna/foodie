375824b6d57562e4e22a81cee3d253316c7b7807
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 02:35:17 +0100

    Update Avatar.js to use shared circle style from SharedStyles.module.css

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
+import sharedStyles from "../styles/SharedStyles.module.css";
 import profileImg from "../assets/images/profile.jpg";
 
 const Avatar = ({ src = profileImg, height = 45, text }) => {
   return (
     <span>
       <img
-        className={styles.Avatar}
+        className={`${styles.avatar} ${sharedStyles.circle}`}
         src={src}
         height={height}
         width={height}
