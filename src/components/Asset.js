43182ce80c51e6a672935c012cf7b5e00d2877af
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-24 03:17:46 +0100

    Update Asset.js to accept className prop for custom styling in PostCreateForm

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -3,9 +3,9 @@ import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 import sharedStyles from "../styles/SharedStyles.module.css";
 
-const Asset = ({ spinner, src, message = "" }) => {
+const Asset = ({ spinner, src, message = "", className = "" }) => {
   return (
-    <div className={`${styles.asset} ${sharedStyles.flexColumnCenter} p-4`}>
+    <div className={`${styles.asset} ${sharedStyles.flexColumnCenter} p-4 ${className}`}>
       {spinner && <Spinner animation="border" />}
       {src && <img src={src} alt={message} className={styles.image} />}
       {message && <p className={`mt-4 ${sharedStyles.message}`}>{message}</p>}
