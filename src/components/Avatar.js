15c387f9bb24508baf776124e68007d641232919
Mustafa Muhammed <mustafa860505@hotmail.com>
2025-03-26 02:33:01 +0100

    Wrap avatar with Link when profileId exists for profile navigation.

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "../styles/Avatar.module.css";
 import sharedStyles from "../styles/SharedStyles.module.css";
 import profileImg from "../assets/images/profile.jpg";
 
-const Avatar = ({ src = profileImg, height = 45, text }) => {
-  return (
-    <span>
+const Avatar = ({ src = profileImg, height = 45, text, profileId }) => {
+  const avatarContent = (
+    <span className={styles.avatarContainer}>
       <img
         className={`${styles.avatar} ${sharedStyles.circle}`}
         src={src}
@@ -15,6 +16,15 @@ const Avatar = ({ src = profileImg, height = 45, text }) => {
       />
       {text}
     </span>
+  );
+
+  // If profileId is provided, make the avatar a clickable link
+  return profileId ? (
+    <Link to={`/profiles/${profileId}`} className={styles.avatarLink}>
+      {avatarContent}
+    </Link>
+  ) : (
+    avatarContent
   );
 };
 
